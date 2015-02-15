9d6cc02b201350557a7734cb30fc447909a499f4
Robert Culliton <robertculliton@Roberts-MacBook-Pro.local>
2015-02-15 17:43:31 -0500

    case class for requestShape simplifies things quite a bit(at least in the success case)

--- app/controllers/Showtime.scala
+++ app/controllers/Showtime.scala
@@ -20,35 +20,14 @@ object Showtime extends Controller {
   
   case class UserNotFoundException(message: String) extends Exception(message)
   
-  def buildRequestMap(input: Request[JsValue], keyNames: List[String]): Map[String,Option[String]] = {
-    var requestInputs:Map[String,Option[String]] = Map()
-    keyNames.foreach { keyName => requestInputs += (keyName -> (input.body \ keyName).asOpt[String]) }
-    requestInputs
-  }
+  case class ExpectedRequest(email: String, url: String, ghostProtocol: Boolean)
   
-  def getMissingParams(requestLookups: Map[String, Option[String]]): Iterable[String] = {
-    for {
-      (key, value) <- requestLookups
-      if !value.isDefined
-    } yield (key)  
-  }   
-  
-  def getValidRequestParams(requestLookups: Map[String, Option[String]]): Array[String] = {
-     val definedRequestValues = for {
-        (key, value) <- requestLookups
-        definedValue <- value
-      } yield (definedValue)
-
-      definedRequestValues.toArray
-  }
-  
-  def kickOffSelenium(emailInput: String, url: String): String = {
+  def kickOffSelenium(emailInput: String, url: String, ghostProtocol: Boolean): String = {
         val account = models.User.fetchByEmail(emailInput)
         if (account == null) {
           throw UserNotFoundException("User not found!")
         }
         
-        val ghostProtocol = true
         val prefix: String = account.getString("prefix")    
         val firstname: String = account.getString("firstname")
         val lastname: String = account.getString("lastname")    
@@ -60,30 +39,20 @@ object Showtime extends Controller {
         val charge = new Charges
         charge.launch(ghostProtocol, url, prefix, firstname, lastname, email, address1, city, state, zip)
   }  
-
+  
   def index() = Action(parse.json) {
      request =>
 
-     val requestBodyParamNames: List[String] = List("email", "url")  
-     val requestLookups: Map[String, Option[String]] = buildRequestMap(request, requestBodyParamNames)
-     val missingParams = getMissingParams(requestLookups)  
-  
-      missingParams match {
-       
-        // if there are no missing parameters then query for user
-        case List() => 
-          val validRequestParams = getValidRequestParams(requestLookups)
-          val endPageTitle: Try[String] = Try(kickOffSelenium(validRequestParams(0), validRequestParams(1)))
-          
-          // if there was an exception retrieving the user then return an error message with a 400
-          endPageTitle match {
-            case Success(endPageTitle) => Ok(endPageTitle)
-            case Failure(ex) => BadRequest(ex.getMessage)            
-          }
+     val requestBodyParamNames: List[String] = List("email", "url", "ghostProtocol")  
+     val incomingRequest = ExpectedRequest((request.body \ "email").as[String], (request.body \ "url").as[String], (request.body \ "ghostProtocol").as[Boolean])
+     val endPageTitle: Try[String] = Try(kickOffSelenium(incomingRequest.email, incomingRequest.url, incomingRequest.ghostProtocol))
+    
+    // if there was an exception retrieving the user then return an error message with a 400
+    endPageTitle match {
+      case Success(endPageTitle) => Ok(endPageTitle)
+      case Failure(ex) => BadRequest(ex.getMessage)            
+    }
 
-        case _ =>
-          BadRequest("missing parameters: " + missingParams.mkString(", "))
-      }
   }
   
 
