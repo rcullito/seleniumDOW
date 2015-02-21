73403b1d7798d0a59583fa31ce7adc47069b932b
Robert Culliton <robertculliton@Roberts-MacBook-Pro.local>
2015-02-21 18:16:02 -0500

    more fields. only remaining ones are headers array and attachments

--- app/controllers/Postmark.scala
+++ app/controllers/Postmark.scala
@@ -45,7 +45,15 @@ object Postmark extends Controller {
           }
         ],
         "OriginalRecipient": "58db53f3a8351b93ce07ff0343e70946@inbound.postmarkapp.com",
-        "Subject": "test2"
+        "Subject": "test2",
+        "MessageID": "996c1fa8-3232-4beb-86f5-2396677b1d2a",
+        "ReplyTo": "",
+        "MailboxHash": "",
+        "Date": "Sat, 21 Feb 2015 09:06:57 -0800 (PST)",
+        "TextBody": "test2\r\n\r\n\u2014\r\nSent from Mailbox",
+        "HtmlBody": "<!DOCTYPE html PUBLIC \"-\/\/W3C\/\/DTD HTML 4.0 Transitional\/\/EN\" \"http:\/\/www.w3.org\/TR\/REC-html40\/loose.dtd\">\r\n<html><body>\r\n<span id=\"mailbox-conversation\"><div>test2<\/div><\/span><div class=\"mailbox_signature\">\r\n<br>&mdash;<br>Sent from <a href=\"https:\/\/www.dropbox.com\/mailbox\">Mailbox<\/a>\r\n<\/div>\r\n<\/body><\/html>\r\n",
+        "StrippedTextReply": "",
+        "Tag": ""   
     }
    """)
    
@@ -63,7 +71,16 @@ object Postmark extends Controller {
       bcc: String,
       bccFull: Seq[full],
       originalRecipient: String,
-      Subject: String
+      subject: String,
+      messageID: String,
+      replyTo: String,
+      mailboxHash: String,
+      date: String,
+      textBody: String,
+      htmlBody: String,
+      strippedTextReply: String,
+      tag: String
+      // TODO headers array and attachments
   ) 
    
   implicit val fullReads: Reads[full] = (
@@ -84,7 +101,15 @@ object Postmark extends Controller {
       (JsPath \ "Bcc").read[String] and 
       (JsPath \ "BccFull").read[Seq[full]] and
       (JsPath \ "OriginalRecipient").read[String] and
-      (JsPath \ "Subject").read[String]
+      (JsPath \ "Subject").read[String] and
+      (JsPath \ "MessageID").read[String] and
+      (JsPath \ "ReplyTo").read[String] and
+      (JsPath \ "MailboxHash").read[String] and 
+      (JsPath \ "Date").read[String] and
+      (JsPath \ "TextBody").read[String] and 
+      (JsPath \ "HtmlBody").read[String] and
+      (JsPath \ "StrippedTextReply").read[String] and
+      (JsPath \ "Tag").read[String]       
     )(postMarkMitt.apply _)
     
   val postMarkResult: JsResult[postMarkMitt] = json.validate[postMarkMitt]
