ef4399c78656aa0df536dc55aebe89ff8e64e623
Robert Culliton <robertculliton@Roberts-MacBook-Pro.local>
2015-02-21 17:53:37 -0500

    adding more fields to the model, learning lessons about sequences

--- app/controllers/Postmark.scala
+++ app/controllers/Postmark.scala
@@ -25,37 +25,76 @@ object Postmark extends Controller {
         "Email": "rob.culliton@gmail.com",
         "Name": "Robert Culliton",
         "MailboxHash": "#47584229"
-      }
+      },
+     "To": "mail@wampum.io",
+     "ToFull": [
+        {
+          "Email": "mail@wampum.io",
+          "Name": "",
+          "MailboxHash": ""
+        }
+      ],
+      "Cc": "",
+      "CcFull": [],
+        "Bcc": "58db53f3a8351b93ce07ff0343e70946@inbound.postmarkapp.com",
+        "BccFull": [
+          {
+            "Email": "58db53f3a8351b93ce07ff0343e70946@inbound.postmarkapp.com",
+            "Name": "",
+            "MailboxHash": ""
+          }
+        ],
+        "OriginalRecipient": "58db53f3a8351b93ce07ff0343e70946@inbound.postmarkapp.com",
+        "Subject": "test2"
     }
    """)
    
   // when nesting case classes have the argument name start with a lower case whereas the case class begins with an upper case
-  case class FromFull(email: String, name: String, mailboxHash: String)
-  case class postMarkMitt(fromName: String, from: String, fromFull: FromFull) 
+  case class full(email: String, name: String, mailboxHash: String)
+  case class postMarkMitt(
+      fromName: String, 
+      from: String, 
+      fromFull: full,
+      to: String,
+      toFull: Seq[full],
+      cc: String,
+      // TODO need to figure out this type, maybe ask Leah or compare with other languages?
+      ccFull: Seq[String],
+      bcc: String,
+      bccFull: Seq[full],
+      originalRecipient: String,
+      Subject: String
+  ) 
    
-  implicit val fromFullReads: Reads[FromFull] = (
+  implicit val fullReads: Reads[full] = (
      (JsPath \ "Email").read[String] and
      (JsPath \ "Name").read[String] and
      (JsPath \ "MailboxHash").read[String]  
-  )(FromFull.apply _)
+  )(full.apply _)
 
   
   implicit val postMarkReads: Reads[postMarkMitt] = (
       (JsPath \ "FromName").read[String] and
       (JsPath \ "From").read[String] and
-      (JsPath \ "FromFull").read[FromFull]
+      (JsPath \ "FromFull").read[full] and
+      (JsPath \ "To").read[String] and
+      (JsPath \ "ToFull").read[Seq[full]] and
+      (JsPath \ "Cc").read[String] and
+      (JsPath \ "CcFull").read[Seq[String]] and
+      (JsPath \ "Bcc").read[String] and 
+      (JsPath \ "BccFull").read[Seq[full]] and
+      (JsPath \ "OriginalRecipient").read[String] and
+      (JsPath \ "Subject").read[String]
     )(postMarkMitt.apply _)
     
-  val locationResult: JsResult[postMarkMitt] = json.validate[postMarkMitt]
-    
-    
-  
+  val postMarkResult: JsResult[postMarkMitt] = json.validate[postMarkMitt]
+
   def sample() = Action {
      request =>
 
-     locationResult match {
+     postMarkResult match {
         case s: JsSuccess[postMarkMitt] => val result = s.get
-          Ok(result.fromFull.mailboxHash)
+          Ok(result.originalRecipient)
         //case e: JsError => println("Errors: " + JsError.toFlatJson(e).toString()) 
       }
     
